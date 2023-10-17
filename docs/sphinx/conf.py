24f3df21e73c2ce4d1602a07d5467602fe83d244
Eric B. Chin <chin23@llnl.gov>
2023-10-16 17:12:54 -0700

    debug output

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -35,7 +35,7 @@ if read_the_docs_build:
 
     # Copy doxygen subfolder to root
     html_extra_path = ['../build/doxygen']
-    print(os.listdir('../build/doxygen'))
+    print(os.listdir('../build'))
 
     # The subfolder is copied to the root after the build completes in
     # the .readthedocs.yaml file
